a68d26db1c54755fc99a8ef8948568ab67a4a376
7h3730B <teoschulze5@gmail.com>
2020-05-01 16:19:40 +0200

    Added some classes

---
+++ classes/bullet.js
@@ -0,0 +1,16 @@
+class Ship {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+    async draw() {
+
+    }
+    getx() {
+        return this.x;
+    }
+    gety() {
+        return this.y;
+    }
+}
