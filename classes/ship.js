9aea436d1565ba6768dab22e608d537b3e8cf644
7h3730B <teoschulze5@gmail.com>
2020-05-01 21:56:03 +0200

    Added Ship functoniality

--- classes/ship.js
+++ classes/ship.js
@@ -7,9 +7,11 @@ class Ship {
         this.radius = this.size / 2;
         this.angle = angle / 180 * Math.PI;
         this.rotation = 0;
-    }
-    async game() {
-        this.angle += this.rotation;
+        this.frictain = 0.7;
+        this.thrust = {
+            x: 0,
+            y: 0
+        }
     }
 
     async draw(ctx) {
@@ -28,6 +30,7 @@ class Ship {
             this.x - this.radius * (Math.cos(this.angle) - Math.sin(this.angle)),
             this.y + this.radius * (Math.sin(this.angle) + Math.cos(this.angle))
         );
+
         ctx.closePath();
         ctx.stroke();
     }
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     game = new Game({
-        fps: 30
+        fps: 60
     });
     game.init();
 };
@@ -12,25 +12,29 @@ class Game {
     constructor(options) {
         this.fps = options.fps;
         this.asteroids = [];
+        this.forward = false;
     }
     init() {
         ctx.canvas.width = window.innerWidth;
         ctx.canvas.height = window.innerHeight;
-        this.ship = new Ship(ctx.canvas.width / 2, ctx.canvas.height / 2, 500, 30, 90);
-        document.addEventListener("keydown", this.keydown);
-        document.addEventListener("keyup", this.keyup);
-        setInterval(this.draw.bind(this), 1000 / 30);
+        this.ship = new Ship(ctx.canvas.width / 2, ctx.canvas.height / 2, 5, 30, 90);
+        document.addEventListener("keydown", this.keydown.bind(this));
+        document.addEventListener("keyup", this.keyup.bind(this));
+        setInterval(this.draw.bind(this), 1000 / this.fps);
+        setInterval(this.game.bind(this), 1000 / this.fps);
     }
     async keydown(ev) {
+        let _this = this;
+
         switch (ev.keyCode) {
             case 37: // left arrow
-                ship.rotation = 50 / 180 * Math.PI / this.fps;
+                _this.ship.rotation = 500 / 180 * Math.PI / this.fps;
                 break;
             case 38: // up arrow
-
+                _this.forward = true;
                 break;
             case 39: // right arrow
-                this.ship.rotation = -50 / 180 * Math.PI / this.fps;
+                _this.ship.rotation = -500 / 180 * Math.PI / this.fps;
                 break;
             default:
                 break;
@@ -38,15 +42,17 @@ class Game {
     }
 
     async keyup(ev) {
+        let _this = this;
+
         switch (ev.keyCode) {
             case 37: // left arrow
-                this.ship.rotation = 0;
+                _this.ship.rotation = 0;
                 break;
             case 38: // up arrow
-
+                _this.forward = false;
                 break;
             case 39: // right arrow
-                this.ship.rotation = 0;
+                _this.ship.rotation = 0;
                 break;
             default:
                 break;
@@ -54,6 +60,7 @@ class Game {
     }
 
     async spawnAsteroids() {
+        if (this.asteroids.length > 20) return;
         this.asteroids.push(new Asteroid(200, 200, 10));
     }
 
@@ -62,7 +69,30 @@ class Game {
         this.asteroids.forEach((asteroid) => {
             asteroid.game();
         });
-        await this.ship.game();
+        this.ship.angle += this.ship.rotation;
+        if (this.forward) {
+            this.ship.thrust.x += this.ship.speed * Math.cos(this.ship.angle) / this.fps;
+            this.ship.thrust.y -= this.ship.speed * Math.sin(this.ship.angle) / this.fps;
+        } else {
+            this.ship.thrust.x -= (0.7 * this.ship.thrust.x / this.fps);
+            this.ship.thrust.y -= (0.7 * this.ship.thrust.y / this.fps);
+        }
+
+        this.ship.x += this.ship.thrust.x;
+        this.ship.y += this.ship.thrust.y;
+
+        if (this.ship.x > ctx.canvas.width) {
+            this.ship.x = 0 - this.ship.radius;
+        }
+        if (this.ship.x < 0 - this.ship.radius) {
+            this.ship.x = ctx.canvas.width;
+        }
+        if (this.ship.y > ctx.canvas.height) {
+            this.ship.y = 0 - this.ship.radius;
+        }
+        if (this.ship.y < 0 - this.ship.radius) {
+            this.ship.y = ctx.canvas.height;
+        }
     }
 
     async draw() {
@@ -72,7 +102,7 @@ class Game {
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
         for (let i = 0; i < _this.asteroids.length; i++) {
-            _this.asteroids[i].draw(ctx);
+            await _this.asteroids[i].draw(ctx);
         }
         await _this.ship.draw(ctx);
     }
